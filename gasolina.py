90013c521d6317d89e2d3fa7ec5bda06f987e12b
fyllype-ytalo <fyllype09@gmail.com>
2022-06-04 00:14:48 +0000

    Arquivos criados originais

---
+++ gasolina.py
@@ -0,0 +1,16 @@
+import csv
+import pandas as pd
+import seaborn as sns
+
+# Convertendo para dataframe
+data = pd.read_csv('./gasolina.csv', sep=',')
+
+# criando gráfico
+with sns.axes_style('whitegrid'): 
+  grafico= sns.relplot(x="dia", y="venda",  kind="line", data=data)
+
+# salvando o grafico em png
+grafico.fig
+grafico.fig.savefig(fname='gasolina.png', bbox_inches='tight')
+
+print('Sucesso!')
